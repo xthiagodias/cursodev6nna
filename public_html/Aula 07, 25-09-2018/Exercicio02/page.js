a43db27f9e33ab7f20c5873c96c5178b46903299
26148269 <26148269@169.254.23.38>
2018-10-02 19:03:27 -0300

    Aula 06 - JavaScript

--- public_html/Aula 07, 25-09-2018/Exercicio02/page.js
+++ public_html/Aula 07, 25-09-2018/Exercicio02/page.js
@@ -1,6 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ function LOG1() {
 
+                var login = document.getElementById("login").value;
+                var senha = document.getElementById("senha").value;
+
+                if (login === "admin" && senha === "123456") {
+
+
+
+
+
+                    // alert("Login Efetuado com Sucesso!");
+                   // window.open('sucesso.html');
+                   window.location.replace("sucesso.html");
+                } else
+                {
+
+                    //  alert("Dados inválidos!");  
+                   // window.open('falha.html');
+                   window.location.replace("falha.html");
+                }
+            }
