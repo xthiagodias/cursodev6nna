079ce20a07a7a429859fed6e9b7c725b5421f9c6
xthiagodias <xthiagodias@192.168.0.5>
2018-10-09 14:08:24 -0300

    Projeto XipBook - Topicos Integradores 2

--- public_html/javascript/page.js
+++ public_html/javascript/page.js
@@ -4,5 +4,75 @@
  * and open the template in the editor.
  */
 
+var login;
+var senha;
 
-$('body').scrollspy({ target: '.anuncios' });
+function salvaCadastroPF() {
+
+    login = document.getElementById("validationCustom08").value;
+    senha = document.getElementById("validationCustom09").value;
+    alert("Cadastro Efetuado com Sucesso! Agora voce pode efetuar seu Login.");
+    window.location.replace("index.html");
+    
+
+    
+
+}
+
+
+
+
+$('body').scrollspy({target: '.anuncios'});
+
+
+function tipoCadastro1() {
+
+    $('#cadPF').show();
+
+}
+
+function tipoCadastro2() {
+
+    $('#cadPF').hide();
+
+}
+
+function tipoCadastro3() {
+
+    $('#cadPJ').hide();
+
+}
+
+function tipoCadastro4() {
+
+    $('#cadPJ').show();
+
+}
+
+function selecionaTipoCadastro(valor) {
+
+    var tipo0 = document.getElementById("tipo0");
+    var visivel = document.getElementById("cadPF").hidden = false;
+    var visivel2 = document.getElementById("cadPJ").hidden = false;
+
+
+    if (tipo0) {
+
+        tipoCadastro2();
+        tipoCadastro3();
+
+    }
+
+    if ((valor === "1")) {
+        tipoCadastro1();
+        tipoCadastro3();
+    }
+
+
+    if (valor === "2") {
+        tipoCadastro2();
+        tipoCadastro4();
+
+
+    }
+}
